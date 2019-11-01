1b03fa4d05e1e9f2fda50db183d6c7949973bd5b
jieyr3Rui <38846961+jieyr3Rui@users.noreply.github.com>
2019-11-01 22:01:16 +0800

    Update consumer.c

--- producer-consumer/consumer.c
+++ producer-consumer/consumer.c
@@ -1,6 +1,8 @@
+#include <math.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <sys/types.h>
+#include <sys/time.h>
 #include <sys/syscall.h>
 #include <semaphore.h>
 #include <stdio.h>
@@ -8,26 +10,28 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <pthread.h>
-#include <math.h>
-#include <sys/time.h>
 #include <stdlib.h>
-sem_t *sem_p = NULL, *sem_c = NULL;
 
+/* args passed to threads*/
 typedef struct _args{
     int id;
     int lambda;
 }args;
 
+/* data structure in shm*/
 typedef struct _data{
+    sem_t *sem_mutex, *sem_p, *sem_c;
     int index;
     int num[20];
 }data;
-data *p = NULL;
-#define COUNT_CONS 3
-pthread_t g_thread[COUNT_CONS];
-pthread_mutex_t g_mutex;
 
-// Possion Function
+/* pointer to shm */
+data *p = NULL;
+
+#define COUNT_PROD 3
+pthread_t g_thread[COUNT_PROD];
+
+/* Possion Function */
 int possion(int lambda){
     int k = 0;
     long double p = 1.0;
@@ -42,54 +46,43 @@ int possion(int lambda){
     return k-1;
 }
 
+
 void* consumer(void* param){
     args* arg = (args*)param;
     int id = arg->id, lambda = arg->lambda;
     while(1){
-        sem_wait(sem_c);
-        pthread_mutex_lock(&g_mutex);
-        
-        //printf("cons[%d]", id);
-        int val = 0;
-        sem_getvalue(sem_c, &val);
+        sem_wait(p->sem_mutex);
+        sem_wait(p->sem_c);
+
         printf("pid:%7d tid:%7ld ", getpid(), syscall(__NR_gettid));
         printf("cons get %2d ",p->num[p->index-1]);
+        /* consume a number to 0 */
         p->num[--p->index] = 0;
-        pthread_mutex_unlock(&g_mutex);
-        // define and limit
+
+        sem_post(p->sem_p);
+        sem_post(p->sem_mutex);
+
+        /* define and limit */
         int poss = possion(lambda) % 10 + 1;
         printf("sleep %ds\n", poss);
         sleep(poss);
-        sem_post(sem_p);
     }
 }
 
 int main(int argc, char **argv){
-    if(argc < 2){ 
+    /* check argment input*/
+    if(argc < 2){
         printf("please input lambda!\n");
         return 0;
     }
-    if(argc > 2){
+    else if(argc > 2){
         printf("please input a number for lambda!\n");
         return 0;
     }
-    pthread_mutex_init(&g_mutex, NULL);
 
+    /* config shm and mat p */
     int shmid = -1;
     key_t key = 0x2235;
-
-    sem_p = sem_open("sem_p", O_CREAT | O_RDWR, 0666, 20);
-    if(sem_p == SEM_FAILED){
-        printf("errno = %d\n", errno);
-        return -1;
-    }
-
-    sem_c = sem_open("sem_c", O_CREAT | O_RDWR, 0666, 0);
-    if(sem_c == SEM_FAILED){
-        printf("errno = %d\n", errno);
-        return -1;
-    }
-    // sem_init(sem_c, 1, 0);
     shmid = shmget(key, sizeof(data), 0666 | IPC_CREAT);
     if(shmid == -1){
         printf("shmget failed\n");
@@ -102,7 +95,8 @@ int main(int argc, char **argv){
         perror("shmat");
         return -1;
     }
-    
+
+    /* create threads */
     for(int ii = 0; ii < COUNT_CONS; ii++){
        args *arg = (args*)malloc(sizeof(args));
        arg->id = ii;
@@ -114,4 +108,3 @@ int main(int argc, char **argv){
     }
     return 0;
 }
-
